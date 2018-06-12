5d7f2a1abdef5461a58d3b0acf831421247d270f
mikaevnikita <n.v.mikaev@gmail.com>
2018-06-12 19:55:35 +0300

    +some changes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-import java.io.IOException;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.logging.*;
 import java.net.InetSocketAddress;
 import java.net.PortUnreachableException;
 import java.nio.channels.*;
@@ -6,133 +10,149 @@ import java.nio.*;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-
 /*
-HTTP 1.0
-NON KEEP-ALIVE
+ HTTP 1.0
+ NON KEEP-ALIVE
+ 
+ +Прикрутить RoadMap
+ +Добавить парсер запросов
+ +Добавить отдачу статического контента (в соотвествии с RoadMap)
+ +Таймауты
+ +Рефакторинг
  */
 
-class HttpConnection{
-    private static final int BUFFER_SIZE = 1024;//1 kb
-    private SocketChannel socketChannel;
-    private ArrayList<ByteBuffer> buffers = new ArrayList<>();
-    private String request;
-    private boolean handled = false;
-
-    public HttpConnection(){
-
-    }
-
-    public HttpConnection(SocketChannel socketChannel){
-        this.socketChannel = socketChannel;
-    }
-
-    public void setSocketChannel(SocketChannel socketChannel) {
-        this.socketChannel = socketChannel;
-    }
-
-    public ArrayList<ByteBuffer> getBuffers() {
-        return buffers;
-    }
-
-    public void setBuffers(ArrayList<ByteBuffer> buffers) {
-        this.buffers = buffers;
-    }
-
-    public String getRequest() {
-        return request;
-    }
-
-    public void setRequest(String request) {
-        this.request = request;
-    }
-
-    public void convertByteRequestToString(){
-        StringBuilder stringBuilder = new StringBuilder();
-        for(ByteBuffer buffer : buffers){
-            String curr = new String(buffer.array());
-            stringBuilder.append(curr);
-        }
-        request = stringBuilder.toString();
-    }
-
-    public SocketChannel getSocketChannel() {
-        return socketChannel;
-    }
-
-    public boolean isHandled() {
-        return handled;
-    }
-
-    public void setHandled(boolean handled) {
-        this.handled = handled;
-    }
-
-    public void addBuffer(ByteBuffer buffer){
-        buffers.add(buffer);
-    }
-
-    public ByteBuffer getLastBuffer(){
-        ByteBuffer lastBuffer;
-        if(buffers.size() ==  0) {
-            lastBuffer = ByteBuffer.allocate(BUFFER_SIZE);//allocate 16kb buffer
-            buffers.add(lastBuffer);
-        }
-        else
-            lastBuffer = buffers.get(buffers.size() == 0 ? 0 : buffers.size() - 1);
-        return lastBuffer;
-    }
-
-    public int getBufferSize(){
-        return BUFFER_SIZE;
-    }
-}
 
 class Server{
+    private static final int BUFFER_SIZE = 1024;//1 kb
+    
+    class HttpConnection{
+        private SocketChannel socketChannel;
+        private ArrayList<ByteBuffer> buffers = new ArrayList<>();
+        private String request;
+        private boolean handled = false;
+        
+        public HttpConnection(){
+            
+        }
+        
+        public HttpConnection(SocketChannel socketChannel){
+            this.socketChannel = socketChannel;
+        }
+        
+        public void setSocketChannel(SocketChannel socketChannel) {
+            this.socketChannel = socketChannel;
+        }
+        
+        public ArrayList<ByteBuffer> getBuffers() {
+            return buffers;
+        }
+        
+        public void setBuffers(ArrayList<ByteBuffer> buffers) {
+            this.buffers = buffers;
+        }
+        
+        public String getRequest() {
+            return request;
+        }
+        
+        public void setRequest(String request) {
+            this.request = request;
+        }
+        
+        public void convertByteRequestToString(){
+            StringBuilder stringBuilder = new StringBuilder();
+            for(ByteBuffer buffer : buffers){
+                String curr = new String(buffer.array());
+                stringBuilder.append(curr);
+            }
+            request = stringBuilder.toString();
+        }
+        
+        public SocketChannel getSocketChannel() {
+            return socketChannel;
+        }
+        
+        public boolean isHandled() {
+            return handled;
+        }
+        
+        public void setHandled(boolean handled) {
+            this.handled = handled;
+        }
+        
+        public void addBuffer(ByteBuffer buffer){
+            buffers.add(buffer);
+        }
+        
+        public ByteBuffer getLastBuffer(){
+            ByteBuffer lastBuffer;
+            if(buffers.size() ==  0) {
+                lastBuffer = ByteBuffer.allocate(BUFFER_SIZE);//allocate 16kb buffer
+                buffers.add(lastBuffer);
+            }
+            else
+                lastBuffer = buffers.get(buffers.size() == 0 ? 0 : buffers.size() - 1);
+            return lastBuffer;
+        }
+        
+        public int getBufferSize(){
+            return BUFFER_SIZE;
+        }
+    }
+    
     private ExecutorService es = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
     private Map<SocketChannel, HttpConnection> connectionsStore = new HashMap<>();
-    private final int port;
+    private int port = 80;
     private ServerSocketChannel serverSocketChannel;
     private Selector selector;
-
+    private final static Logger logger = Logger.getLogger(Server.class.getName());
+    static{
+        logger.setLevel(Level.INFO);
+        logger.addHandler(new ConsoleHandler());
+        try {
+            logger.addHandler(new FileHandler("./log.log"));
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "Cant initialize logfile: ", ex);
+        }
+    }
+    
     public Server(int port){
         this.port = port;
     }
-
-    public void closeConnection(HttpConnection connection) throws IOException {
+    
+    public synchronized void closeConnection(HttpConnection connection) throws IOException {
         connection.setHandled(true);
         SocketChannel socketChannel = connection.getSocketChannel();
         socketChannel.close();
         connectionsStore.remove(connection);
     }
-
+    
     public void serve(HttpConnection connection){
         if(connection.isHandled())
             return;
-
+        
         es.submit(new Runnable() {
             @Override
             public void run() {
                 try {
                     connection.convertByteRequestToString();
-                    System.out.println(connection.getRequest());//DEBUG
-
+                    logger.log(Level.INFO, "Request: ", connection.getRequest());//DEBUG
+                    
                     String answer = "Hello, World";
                     SocketChannel socketChannel = connection.getSocketChannel();
-                    socketChannel.write(ByteBuffer.wrap(new String("HTTP/1.0 200 OK\n" +
-                                                                    "Content-Length: " + answer.length() + " \n" +
-                                                                    "Content-Type: text/html\n" +
-                                                                    "Connection: Closed\n\n" +
-                                                                    answer).getBytes()));
+                    socketChannel.write(ByteBuffer.wrap(("HTTP/1.0 200 OK\n" +
+                                                         "Content-Length: " + answer.length() + " \n" +
+                                                         "Content-Type: text/html\n" +
+                                                         "Connection: Closed\n\n" +
+                                                         answer).getBytes()));
                     closeConnection(connection);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                } catch (IOException ex) {
+                    logger.log(Level.WARNING, "handle client error: ", ex);
                 }
             }
         });
     }
-
+    
     public void handleRead(SocketChannel socketChannel) throws IOException {
         HttpConnection connection = connectionsStore.get(socketChannel);
         ByteBuffer lastBuffer = connection.getLastBuffer();
@@ -156,7 +176,7 @@ class Server{
             }
         }
     }
-
+    
     public void handleConnect() throws IOException {
         SocketChannel socketChannel = (SocketChannel) serverSocketChannel.accept();
         socketChannel.configureBlocking(false);
@@ -164,20 +184,20 @@ class Server{
         HttpConnection connection = new HttpConnection(socketChannel);
         connectionsStore.put(socketChannel, connection);
     }
-
+    
     public void handle() throws IOException {
         serverSocketChannel = ServerSocketChannel.open();
         serverSocketChannel.bind(new InetSocketAddress(port));
-
+        
         selector = Selector.open();
         serverSocketChannel.configureBlocking(false);
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-
+        
         while(true){
             int selectionKeysCount = selector.select();
             if(selectionKeysCount == 0)
                 continue;
-
+            
             Set<SelectionKey> selectedKeys = selector.selectedKeys();
             Iterator<SelectionKey> it = selectedKeys.iterator();
             while(it.hasNext()){
@@ -187,7 +207,7 @@ class Server{
                         handleConnect();
                     }
                     catch(IOException ex){
-                        ex.printStackTrace();
+                        logger.log(Level.SEVERE, "Connect event error", ex);
                     }
                 }
                 else if(currSelectionKey.isReadable()){
@@ -195,7 +215,7 @@ class Server{
                         handleRead((SocketChannel) currSelectionKey.channel());
                     }
                     catch(IOException ex){
-                        ex.printStackTrace();
+                        logger.log(Level.SEVERE, "Read event error: ", ex);
                     }
                 }
             }
@@ -225,4 +245,3 @@ public class Main {
         }
     }
 }
-
